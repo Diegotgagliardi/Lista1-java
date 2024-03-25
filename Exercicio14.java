f8196a42c001394743dda351bd473f7d42864dbf
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-03-25 12:15:10 -0300

    Exercicio 14

---
+++ Exercicio14.java
@@ -0,0 +1,10 @@
+public class Exercicio14 {
+    public static void executar() {
+    double a1 = 10;
+    double n = 7;
+    double r = 3;
+
+    double an = a1 + (n - 1) * r;
+    System.out.println("Resultado: " + an);
+    }
+}
