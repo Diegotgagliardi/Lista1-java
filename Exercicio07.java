8881583055ec3c303343a9527ebc04d75ce6f524
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-03-24 22:00:16 -0300

    Exercicio 07

---
+++ Exercicio07.java
@@ -0,0 +1,13 @@
+public class Exercicio07 {
+    
+    public static void executar() {
+        int num = 300;
+
+        if (num >= 100 && num<= 200){
+        System.out.println("Dentro do intervalo");
+        }
+        else {
+        System.out.println("Fora do intervalo");
+        }
+    }
+}
