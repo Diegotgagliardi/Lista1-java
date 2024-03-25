f529bab47e06f4a158ec9fb5cec948e464fdbf53
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-03-24 21:54:51 -0300

    Exercicio 03

---
+++ Exercicio03.java
@@ -0,0 +1,14 @@
+    public class Exercicio03 {
+        
+        public static void executar() {
+            int var1 = 10;
+            int var2 = 10;
+
+            if (var1 > var2)
+                System.out.println("Var 1 é maior");
+            else if (var1 < var2)
+                System.out.println("Var 2 é maior");
+            else
+                System.out.println("Inválido, não podem ser números iguais");
+        }        
+    }
